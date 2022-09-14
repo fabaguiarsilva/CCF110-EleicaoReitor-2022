2ec0dd6e67e1d331543d05cebbc0365a1e01d658
Fabrício <fabaguiarsilva@gmail.com>
2022-09-14 10:32:29 -0300

    commit inicial

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fabriciosilva
+ *
+ * Created on September 14, 2022, 10:22 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+
+    return (EXIT_SUCCESS);
+}
+
