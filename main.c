9710b3e4d0a1102a03582a2a2be1f36de882737e
Fabrício <fabricio.silva@cinnecta.com>
2022-09-14 11:12:08 -0300

    Versão com 3 candidatos e 3 eleitores

--- main.c
+++ main.c
@@ -19,11 +19,61 @@ int main() {
     
     int voto;
     
-    printf("Digite o seu voto! ");
-    scanf("%d",&voto);
-    printf("Voto: %d\n######",voto);
     
-
+    //Ler Votos
+    printf("Digite o voto do eleitor 1! ");
+    scanf("%d",&voto);
+    if(voto == 1){
+        nVotosCandidato1++;
+    }else if(voto == 2){
+        nVotosCandidato2++;
+    }else if(voto == 3){
+        nVotosCandidato3++;
+    }
+    
+    
+    
+    printf("Digite o voto do eleitor 2! ");
+    scanf("%d",&voto);
+    if(voto == 1){
+        nVotosCandidato1++;
+    }else if(voto == 2){
+        nVotosCandidato2++;
+    }else if(voto == 3){
+        nVotosCandidato3++;
+    }
+    
+    printf("Digite o voto do eleitor 3! ");
+    scanf("%d",&voto);
+    if(voto == 1){
+        nVotosCandidato1++;
+    }else if(voto == 2){
+        nVotosCandidato2++;
+    }else if(voto == 3){
+        nVotosCandidato3++;
+    }  
+    
+    
+    //Apurar votação
+    if(nVotosCandidato1 > nVotosCandidato2
+       && nVotosCandidato1 > nVotosCandidato3){
+        printf("Candidato 1 é o vencedor com %d "
+                "voto(s)",nVotosCandidato1);        
+    }else if(nVotosCandidato2 > nVotosCandidato1
+       && nVotosCandidato2 > nVotosCandidato3){
+        printf("Candidato 2 é o vencedor com %d "
+                "voto(s)",nVotosCandidato2);               
+    }else if(nVotosCandidato3 > nVotosCandidato1
+       && nVotosCandidato3 > nVotosCandidato2){
+        printf("Candidato 3 é o vencedor com %d "
+                "voto(s)",nVotosCandidato3);                       
+    }
+    
+    
+    
+    
+    
+    
     
     return (EXIT_SUCCESS);
 }
